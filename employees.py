4be34f1a83f36c27a7147a6690d02e3ff9d12391
Borsapv <marian.borsiczky@gmail.com>
2021-04-11 00:35:44 +0200

    Added functions to manage stitky related to employee

--- employees.py
+++ employees.py
@@ -14,13 +14,48 @@ mydb = mysql.connector.connect(
 # create a cursor
 c = mydb.cursor()
 
-# function to show stitky needs to be asigned
-def unassigned_stitky():
+# function to show stitky related to assigned to employee
+def assigned_stitky():
     return
 
-# function to show stitky related to assigned to emploeyy
-def unassigned_stitky():
-    return
+
+# function to assign stitek to employee
+def assign_stitek(employee_id, assign_box):
+    sql_cmd = "UPDATE stitky SET empl_id = %s WHERE stitek_ID = %s"
+    c.execute(sql_cmd, (employee_id, assign_box,))
+
+    test_sql_cmd = "SELECT * FROM stitky WHERE empl_id = %s"
+    c.execute(test_sql_cmd, (employee_id,))
+    assigned = c.fetchall()
+    for ass in assigned:
+        print(ass)
+
+    mydb.commit()
+
+# function to show stitky needs to be assigned
+def unassigned_stitky(screen, num, employee_id):
+    num += 1
+    volne_stitky_lbl = Label(screen, text="Nepřiřazené štítky").grid(row=num, column=0, columnspan=2, pady=15)
+
+    c.execute("SELECT * FROM stitky")
+    table = c.fetchall()
+    for st_num, stitek in enumerate(table):
+        col3 = 0
+        num += 1
+        for stit in stitek:
+            stit_list = Label(screen, text=stit)
+            stit_list.grid(row=num, column=col3, padx=5, stick=W)
+            col3 += 1
+
+    num += 1
+    # button to assign stitek
+    assign_lbl = Label(screen, text="ID štítku")
+    assign_lbl.grid(row=num, column=0, columnspan="2", stick=W)
+    assign_box = Entry(screen, width="5")
+    assign_box.grid(row=num, column=2, stick=W)
+    assign_btn = Button(screen, text="Potvrdit", command=lambda: assign_stitek(employee_id, assign_box.get()))
+    assign_btn.grid(row=num, column=3, columnspan="2", stick=W)
+
 
 # function to show employee detail
 def employee_detail(employee_id):
@@ -35,7 +70,7 @@ def employee_detail(employee_id):
 
     detail_screen = Tk()
     detail_screen.title(name)
-    detail_screen.geometry("400x400")
+    detail_screen.geometry("600x400")
 
     # show list of employees related to ID (should be one)
     for num, detail in enumerate(details):
@@ -45,6 +80,7 @@ def employee_detail(employee_id):
             det_list.grid(row=num, column=col2, padx=5, stick=W)
             col2 += 1
 
+    unassigned_stitky(detail_screen, num, employee_id)
 
 # show all employees
 def show_all_employees():
